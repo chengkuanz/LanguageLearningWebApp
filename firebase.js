614feefa1a4e9023dc0c60d88ceebbdfb2452bb7
Chengkuan Zhao <95447572+chengkuanz@users.noreply.github.com>
2024-03-20 18:41:29 -0400

    working

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApps, getApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-
+import { getFirestore } from 'firebase/firestore'
+import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDhvl-1OaLVQe7BS745YJRm6719Zxkk-cc",
+  apiKey: "AIzaSyDhvl-1OaLVQe7BS745YJRm6719Zxkk-cc",
   authDomain: "languagelearning-5bf8f.firebaseapp.com",
   projectId: "languagelearning-5bf8f",
   storageBucket: "languagelearning-5bf8f.appspot.com",
@@ -13,11 +13,8 @@ const firebaseConfig = {
   measurementId: "G-K9BL52QC4M"
 }
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-
 export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
 export const storage = getStorage(app);
